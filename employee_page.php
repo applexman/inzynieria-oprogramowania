0111534184b51c15c1ffab1c5e0f1664b78d8a33
Marcin Koterba <marcin.koterba1@gmail.com>
2023-06-03 16:14:34 +0200

    employee_page
    Głównie fron-end ale jest też trochę back-endu.

--- employee_page.php
+++ employee_page.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if ((!isset($_SESSION['permissions'])) || ($_SESSION['permissions'] != 1)) {
+if ((!isset($_SESSION['permissions'])) || ($_SESSION['permissions'] != 1 && $_SESSION['permissions'] != 2)) {
     header('Location: login_page.php');
     exit();
 }
@@ -23,7 +23,7 @@ if ((!isset($_SESSION['permissions'])) || ($_SESSION['permissions'] != 1)) {
 
     <div class="container text-center">
         <div class="row">
-            <div class="col col-md-5">
+            <div class="col col-md-7">
                 <h2>Products</h2>
                 <table class="table table-striped table-hover">
                     <thead>
@@ -31,8 +31,8 @@ if ((!isset($_SESSION['permissions'])) || ($_SESSION['permissions'] != 1)) {
                             <th scope="col">id#</th>
                             <th scope="col">Name</th>
                             <th scope="col">Description</th>
-                            <th scope="col">Img</th>
                             <th scope="col">Price</th>
+                            <th scope="col">Category</th>
                             <th scope="col">Edit</th>
                         </tr>
                     </thead>
@@ -44,7 +44,10 @@ if ((!isset($_SESSION['permissions'])) || ($_SESSION['permissions'] != 1)) {
                         function getProducts($connection)
                         {
                             $products = array();
-                            $sql = "SELECT * FROM products";
+                            $sql = "SELECT products.id, products.name, products.description, products.price, categories.name as categories
+                            FROM products, categories
+                            WHERE products.categoryId= categories.id
+                            ORDER BY products.id;";
                             $result = $connection->query($sql);
                             if ($result->num_rows > 0) {
                                 while ($row = $result->fetch_assoc()) {
@@ -61,14 +64,14 @@ if ((!isset($_SESSION['permissions'])) || ($_SESSION['permissions'] != 1)) {
                                 <th scope="row">' . $product['id'] . '</th>
                                     <td>' . $product['name'] . '</td>
                                     <td>' . $product['description'] . '</td>
-                                    <td>' . $product['img'] . '</td>
-                                    <td>' . $product['price'] . '</td>
+                                    <td>' . $product['price'] . ' zł</td>
+                                    <td>' . $product['categories'] . '</td>
                                     <td><a class="btn btn-outline-danger shadow btn-sm" role="button" href="edit_product.php?id=' . $product['id'] . '">Edit</a>
                                 </tr>';
                         }
 
                         ?>
-                        <tr><a class="btn" href="add_product.php">Dodaj produkt</a></tr>
+                        <tr><a class="btn btn-outline-info btn-sm" href="add_product.php">Dodaj produkt</a></tr>
                     </tbody>
                 </table>
             </div>
@@ -81,6 +84,8 @@ if ((!isset($_SESSION['permissions'])) || ($_SESSION['permissions'] != 1)) {
                             <th scope="col">User ID</th>
                             <th scope="col">Total</th>
                             <th scope="col">Name | Quantity</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Zmień status</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -102,7 +107,9 @@ if ((!isset($_SESSION['permissions'])) || ($_SESSION['permissions'] != 1)) {
                         function getOrdersDetail($connection)
                         {
                             $ordersD = array();
-                            $sql = "SELECT products.name AS productName, orderdetail.quantity AS quantity, orderdetail.idOrder FROM `products`, `orderdetail` WHERE products.id=orderdetail.idProduct";
+                            $sql = "SELECT products.name AS productName, orderdetail.quantity AS quantity, orderdetail.idOrder 
+                            FROM `products`, `orderdetail`
+                            WHERE products.id=orderdetail.idProduct";
                             $result = $connection->query($sql);
                             if ($result->num_rows > 0) {
                                 while ($row = $result->fetch_assoc()) {
@@ -129,6 +136,8 @@ if ((!isset($_SESSION['permissions'])) || ($_SESSION['permissions'] != 1)) {
 
                             echo '
                                 </td>
+                                <td>' . $order['status'] . '</td>
+                                <td><a class="btn btn-outline-danger shadow btn-sm" role="button" href="change_status_script.php?id=' . $order['id'] . '">Zmień</a>
                             </tr>';
                         }
 
@@ -136,56 +145,9 @@ if ((!isset($_SESSION['permissions'])) || ($_SESSION['permissions'] != 1)) {
                     </tbody>
                 </table>
             </div>
-            <div class="col col-md-2">
-                <h2>Users</h2>
-                <table class="table table-striped table-hover table-sm">
-                    <thead>
-                        <tr>
-                            <th scope="col">id#</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Stopień uprawnień</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        require_once "database_connect.php";
-
-                        function getUsers($connection)
-                        {
-                            $products = array();
-                            $sql = "SELECT * FROM users";
-                            $result = $connection->query($sql);
-                            if ($result->num_rows > 0) {
-                                while ($row = $result->fetch_assoc()) {
-                                    $products[] = $row;
-                                }
-                            }
-                            return $products;
-                        }
-
-
-                        foreach (getUsers($connection) as $user) {
-                            $permissions = ($user['permissions'] == 1) ? 'Admin' : (($user['permissions'] == 2) ? 'Pracownik' : 'Klient');
-                            echo
-                            '<tr>
-                                    <th scope="row">' . $user['id'] . '</th>
-                                    <td>' . $user['email'] . '</td>
-                                    <td>' . $permissions . '</td>
-                                </tr>';
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
         </div>
     </div>
     </div>
-
-
-
-
-
-
 
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
